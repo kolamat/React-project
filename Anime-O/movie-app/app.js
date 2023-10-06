8f718b8b343e9dd64d36f19c9b4d534dfa501643
kolamat <132449653+kolamat@users.noreply.github.com>
2023-10-06 10:50:23 -0700

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   Anime-O/movie-app/app.js
    	modified:   Anime-O/movie-app/index.html
    	modified:   Anime-O/movie-app/style.css

---
+++ Anime-O/movie-app/app.js
@@ -0,0 +1,8 @@
+// const API_KEY =
+const image_path = "https//image.tmdb.org/t/p/w1280";
+const trailer_path = "https//www.youtube.com/watch?v=";
+
+const input = document.querySelector(".search input");
+const btn = document.querySelector(".search button");
+const main_grid_title = document.querySelector(".favorites h1");
+const main_grid = document.querySelector(".favorites .movies-grid");
